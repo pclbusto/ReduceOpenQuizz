4f4465cabe7662998e5f06e3cb29291a991114de
Isidoro Legido Martínez <islegmar@gmail.com>
2022-03-04 12:13:49 +0100

    Refactor and still unestable multiplayer

---
+++ src/appinventor/ai_islegmar/ICanBelieveItSeveralScreens/Screen1.scm
@@ -0,0 +1,4 @@
+#|
+$JSON
+{"authURL":["ai2.appinventor.mit.edu"],"YaVersion":"213","Source":"Form","Properties":{"$Name":"Screen1","$Type":"Form","$Version":"30","AppName":"ICanBelieveIt","TitleVisible":"False","Uuid":"0","VersionName":"0.9"}}
+|#
